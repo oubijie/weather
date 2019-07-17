47e5ce25d76098898bd406d05f931d3824389aa6
oubijie <oubijie@139.com>
2019-07-17 11:18:21 +0800

    接口servlet

--- src/main/java/com/veryoo/weather/servlet/WeatherServlet.java
+++ src/main/java/com/veryoo/weather/servlet/WeatherServlet.java
@@ -1,5 +1,46 @@
 package com.veryoo.weather.servlet;
 
-public class WeatherServlet {
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSON;
+import com.veryoo.weather.service.WeatherService;
+import com.veryoo.weather.service.impl.WeatherServiceImpl;
+
+
+@WebServlet(value={"/api/weather"})
+public class WeatherServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("application/Json");
+		
+		WeatherService w = new WeatherServiceImpl();
+		
+		Map map = null;
+		
+		String cityCode = req.getParameter("cityCode");
+		if(cityCode != null && !cityCode.trim().equals("")) {
+			map = w.getWeatherDataByCityCode(cityCode);
+		}else {
+			String cityName = req.getParameter("cityName");
+			if(cityName != null && !cityName.trim().equals("")) {
+				map = w.getWeatherDataByCityName(cityName);
+			}
+		}
+		
+		resp.getWriter().write(JSON.toJSONString(map));
+		resp.getWriter().flush();
+	}
+	
+	
 
 }
